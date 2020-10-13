790b241acb46c92f7a572c35309a252f4ee16785
thomaslwk <leethomaswg@gmail.com>
2020-10-13 20:05:53 +0800

    Import gunicorn for heroku hosting

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,11 @@
 import os 
 from flask import Flask 
 
+
 def create_app(test_config=None):
     # create and configure app 
     app = Flask(__name__, instance_relative_config=True)
-    
+
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/',endpoint='index.html')
