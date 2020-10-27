9676f20746d5b86717309e272efca7f7417a7398
thomaslwk <leethomaswg@gmail.com>
2020-10-27 23:01:23 +0800

    Import images, and setup API endpoint for image consumption for telegram bot

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for, send_file
 from source.denguegraph import avgrainfalllist, avgdenguelist, nea_data_big_c, nea_data_small_c
 from source.matplot_functions import linear_reg_chart
 from source.denguemap import make_folium_map, list_compare
@@ -18,10 +18,24 @@ def index():
                            neadatasmall=nea_data_small_c(),
                            )
     
-
 @bp.route('/map')
 def geomap():
     # return make_folium_map()
     make_folium_map().save('flaskr/templates/map.html')
     return render_template('blog/geomap.html',)
+
+@bp.route('/linear_reg_img')
+def get_lr_img():
+    filename = 'static/images/LR_graph.png'
+    return send_file(filename)
+
+@bp.route('/map_img')
+def get_mp_img():
+    filename = 'static/images/Map.png'
     
+    return send_file(filename)
+
+@bp.route('/rainfall_img')
+def get_rain_img():
+    filename = 'static/images/Rainfall_graph.png'
+    return send_file(filename)
