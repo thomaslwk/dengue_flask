1c23f87a210465e3a5658d232170951028ba5d6f
thomaslwk <leethomaswg@gmail.com>
2020-10-30 14:18:44 +0800

    Update webscraper, documentation

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -7,6 +7,7 @@ from werkzeug.exceptions import abort
 
 bp = Blueprint('blog', __name__)
 
+# -- Return route for main elements -- # 
 @bp.route('/', methods=('GET','POST'))
 def index():
     return render_template('blog/admin.html',
@@ -17,24 +18,27 @@ def index():
                            neadatabig=nea_data_big_c(),
                            neadatasmall=nea_data_small_c(),
                            )
-    
+# -- Return route for map -- #
 @bp.route('/map')
 def geomap():
-    # return make_folium_map()
     make_folium_map().save('flaskr/templates/map.html')
     return render_template('blog/geomap.html',)
 
+# -- Endpoint for Serving graph information back to Telegram Bot -- # 
+#
+# -- Return Linear Reg Graph Image -- #
 @bp.route('/linear_reg_img')
 def get_lr_img():
     filename = 'static/images/LR_graph.png'
     return send_file(filename)
 
+# -- Return Folium Map Image -- # 
 @bp.route('/map_img')
 def get_mp_img():
     filename = 'static/images/Map.png'
-    
     return send_file(filename)
 
+# -- Return Dengue and Rainfall Image -- # 
 @bp.route('/rainfall_img')
 def get_rain_img():
     filename = 'static/images/Rainfall_graph.png'
